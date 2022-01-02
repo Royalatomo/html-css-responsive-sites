79f497cb25ec5064d0d274249471fc26d63445f2
BINGOO <54973413+Royalatomo@users.noreply.github.com>
2022-01-02 19:58:37 +0530

    Add files via upload

---
+++ tea station/js/script.js
@@ -0,0 +1,7 @@
+function showHtml(){
+    document.getElementsByClassName('navigation')[0].classList.add('showNav');
+}
+
+function hideHtml(){
+    document.getElementsByClassName('navigation')[0].classList.remove('showNav');
+}
